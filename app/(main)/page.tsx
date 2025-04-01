29bd95ec5495dd841fbfca03fc724aca55a1dbb0
varie <myokhant122021@gmail.com>
2025-04-01 14:43:39 +0700

    UPD: fix

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import LoadingScreen from "@/components/sections/loading-creen";
-import HomeSection from "@/components/sections/home";
-import ProjectsSection from "@/components/sections/projects";
-import Technologies from "@/components/sections/technologies";
-import AboutSection from "@/components/sections/about";
 import dynamic from 'next/dynamic';
-
 const ContactSection = dynamic(() => import('@/components/sections/contact'), { ssr: false });
+const HomeSection = dynamic(() => import('@/components/sections/home'), { ssr: false });
+const ProjectsSection = dynamic(() => import('@/components/sections/projects'), { ssr: false });
+const Technologies = dynamic(() => import('@/components/sections/technologies'), { ssr: false });
+const AboutSection = dynamic(() => import('@/components/sections/about'), { ssr: false });
+const LoadingScreen = dynamic(() => import('@/components/sections/loading-creen'), { ssr: false });
+
 export default function Home(){
     return (
     <>
