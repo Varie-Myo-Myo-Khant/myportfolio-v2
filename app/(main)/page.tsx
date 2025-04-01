7fad3cd1d42bbbc89d8974f466a36eb0be9d751c
varie <myokhant122021@gmail.com>
2025-04-01 14:39:17 +0700

    UPD: fix

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import LoadingScreen from "@/components/sections/loading-creen";
 import HomeSection from "@/components/sections/home";
-import ContactSection from "@/components/sections/contact";
 import ProjectsSection from "@/components/sections/projects";
 import Technologies from "@/components/sections/technologies";
 import AboutSection from "@/components/sections/about";
+import dynamic from 'next/dynamic';
 
+const ContactSection = dynamic(() => import('@/components/sections/contact'), { ssr: false });
 export default function Home(){
     return (
     <>
