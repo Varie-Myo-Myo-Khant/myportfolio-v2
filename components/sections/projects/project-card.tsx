866c58824777ad7c53840a611283bd76a2bd143d
varie <myokhant122021@gmail.com>
2025-04-02 12:39:59 +0700

    UPD: fix

--- components/sections/projects/project-card.tsx
+++ components/sections/projects/project-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { ExternalLink, Globe } from "lucide-react";
+import { ExternalLink, Globe ,X} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useSession } from "next-auth/react"; // Assuming you're using next-auth for authentication
@@ -41,11 +41,11 @@ export default function ProjectCard({ project }: props) {
   };
 
   return (
-    <div className="w-full lg:max-w-[400px] max-w-fit rounded-2xl bg-muted border px-3 sticky top-8">
+    <div className="w-full lg:max-w-[400px] max-w-fit rounded-2xl bg-muted border px-3 sticky top-4">
       <div className="flex justify-between flex-shrink-0 px-4 pt-2">
         <div className="flex items-center gap-2 text-muted-foreground">
           <Globe size={18} />
-          <span>Web-Page</span>
+          <span>{project.confidential? "Work Project":"Freelance Project"}</span>
         </div>
         <div className="flex items-center gap-2">
           <span className="block rounded-full size-3 bg-green-500 ml-auto" />
@@ -55,15 +55,20 @@ export default function ProjectCard({ project }: props) {
       </div>
       <div className="group relative h-[200px] overflow-hidden cursor-pointer rounded-lg m-2 border">
         <Image className="size-full object-cover object-top" src={project.image || '/img/demo.png'} alt={project.title} width={400} height={400} />
-        <Link href={project.sourcelink} target="_blank" className="size-full bg-black/50 absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity" />
+        <Link href="#" className="size-full bg-black/50 absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity" />
         <ExternalLink size={24} className="absolute top-4 right-4 opacity-90 hidden group-hover:block" />
       </div>
-      <div className="px-4 py-2 w-full">
-        <h2 className="text-xl capitalize font-bold my-3">{project.title}</h2>
-        <p className="text-muted-foreground h-[150px] overflow-hidden">{project.description}</p>
-        <div className="space-x-2 my-7">
+      <div className="px-4 py-2 text-xs w-full">
+        <h2 className="text-lg capitalize font-bold my-3">{project.title}</h2>
+        <p className="text-muted-foreground">
+         <span className="text-primary-foreground"> Tech Stack :</span> {
+            project.techstack.toString()
+            }
+        </p>
+        <p className="text-muted-foreground h-[50] max-h-[50] overflow-hidden"> 
+          <span className="text-primary-foreground"> Overview :</span> {project.description}</p>
+        <div className="space-x-2 my-5">
           {project.confidential ? (
-            // If the project is confidential, check if the password is correct or the user is authenticated
             passwordCorrect || session ? (
               // Show the Live view link if the user is authenticated or password is correct
               <Button asChild variant="gradientDefault" >
@@ -81,44 +86,47 @@ export default function ProjectCard({ project }: props) {
                   Require Credentails to View Details
                 </Button>
 
-                {/* Tailwind Popup Modal for Password */}
                 {showPasswordForm && (
-                  <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
-                    <div className="bg-white p-6 rounded-lg w-[90%] max-w-md">
-                      <h3 className="text-xl mb-4">Enter Project Password</h3>
-                      {/* Show an error alert if the password is incorrect */}
+                  <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    <div className="bg-white text-md p-5 rounded-lg w-[90%] max-w-md">
+                      <div className="flex justify-between items-center">
+                      <h3 className=" text-black ">Enter Project Password</h3>
+                        
+                        <Button  onClick={() => setShowPasswordForm(false)} size="icon" className="bg-transparent text-muted hover:bg-transparent hover:text-primary-foreground p-0 m-0 w-auto">
+                          <X size={20} />
+                        </Button>
+                      </div>
+                      
                       {passwordError && (
-                        <div className="bg-red-100 border border-red-500 text-red-700 p-3 rounded mb-4">
+                        <div className="bg-red-100 border text-xs border-red-500 text-red-700 p-3 rounded mb-4">
                           {passwordError}
                         </div>
                       )}
                       <form onSubmit={handlePasswordSubmit}>
+                      <div className="flex justify-between items-center gap-2 my-4">
                         <input
                           type="password"
                           placeholder="Enter password"
                           value={password}
                           onChange={(e) => setPassword(e.target.value)}
-                          className="p-2 border text-black rounded w-full mb-4"
+                          className="p-2 border text-black rounded-md w-[80%]"
                           required
                         />
-                        <Button
+                        <Button className="text-white bg-primary-foreground hover:text-primary-foreground hover:bg-transparent border-2 border-primary-foreground rounded-md hover:border-primary-foreground"
                           type="submit"
-                          variant="gradientDefault"
-                          className="w-full"
                         >
-                          Submit Password
+                          Submit
                         </Button>
+                        
+                        </div>
+                        <p className="text-xs my-2 text-primary-foreground">
+                        No Passowrd?&nbsp;
+                        <Link href="/#contact" onClick={() => setShowPasswordForm(false)} className="underline">Request access</Link>
+                        </p>
+                        
                       </form>
-                      <p className="mt-2 text-sm">
-                        <Link href="/contact" className="text-blue-500">Request access</Link>
-                      </p>
-                      <Button
-                        variant="ghost"
-                        className="mt-4 w-full"
-                        onClick={() => setShowPasswordForm(false)}
-                      >
-                        Close
-                      </Button>
+                      
+                      
                     </div>
                   </div>
                 )}
