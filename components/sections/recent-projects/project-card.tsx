7a792edf1b785019a3a5a25bb31d2bf5233bf84d
varie <myokhant122021@gmail.com>
2025-04-20 21:02:41 +0700

    UPD: Fix

--- components/sections/recent-projects/project-card.tsx
+++ components/sections/recent-projects/project-card.tsx
@@ -79,7 +79,7 @@ export default function ProjectCard({ project }: props) {
                 {/* If password is not entered correctly, show the Enter Password button */}
                 <Button className="text-xs"
                   variant="gradientDefault"
-                  onClick={() => setShowPasswordForm(project.id.toString())}
+                  onClick={() => setShowPasswordForm({ id: project.id.toString() })}
                 >
                   Require Crendentials to View
                 </Button>
